21662728526492c341386809cd7a06ec8ac8e1cd
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-04-13 15:52:04 +0300

    complete

--- src/helpers/fetchCountries.js
+++ src/helpers/fetchCountries.js
@@ -1,17 +1,14 @@
-import Notiflix from 'notiflix';
+
 const searchParams = new URLSearchParams({
   fields: 'name,languages,flags,population,capital',
 });
 
 export default function fetchCountries(name) {
-  fetch(`https://restcountries.com/v3.1/name/${name}?${searchParams}`, )
-    .then(response => response.json())
-    .then(data => {
-      if (data.length > 10) {
-        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      };
-      return console.log(data)
+  return fetch(`https://restcountries.com/v3.1/name/${name}?${searchParams}`)
+    .then(response => {
+      if (response.status === 404) {
+        throw new Error()
+      }
+      return response.json()
     })
-    // .catch
-  
 }
--- src/index.js
+++ src/index.js
@@ -6,41 +6,61 @@ import fetchCountries from './helpers/fetchCountries';
 const searchField = document.querySelector('#search-box');
 const countryMatchedList = document.querySelector('.country-list')
 const oneCountryInfo = document.querySelector('.country-info');
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 500;
 
-searchField.addEventListener('input', debounce(magic, DEBOUNCE_DELAY));
+searchField.addEventListener('input', debounce(createMarkup, DEBOUNCE_DELAY));
 
 
-function magic(evt) {
+function createMarkup(evt) {
+  resetMarkup();
   if (evt.target.value.trim() === '') {
     return 
   };
-  fetchCountries(evt.target.value);
+  fetchCountries(evt.target.value).then(chooseMarkupType)
+    .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name.'))
+};
+
+function chooseMarkupType(dataArr) {
+  if (dataArr.length === 1) {
+      return makeOneCountyInfo(dataArr)
+    } else if (dataArr.length >= 2 && dataArr.length <= 10) {
+      return makeList(dataArr);
+    } else if (dataArr.length > 10) {
+      return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }
 };
 
 function makeList(dataArr) {
+  resetMarkup()
   const countyList = dataArr.map(country => `<li>
-        <p>
-          <img src="${country.flags.svg}" alt="флаг країни ${country.name.official}">
-          <span>${country.name.official}</span>
-        </p>
+        <img class="country-flag" src="${country.flags.svg}" alt="флаг країни ${country.name.official}">
+        <p>${country.name.official}</p>
       </li>`).join('');
   countryMatchedList.innerHTML = countyList;
 };
 
-function makeOneCountyOnfo(dataArr) {
+function makeOneCountyInfo(dataArr) {
+  resetMarkup()
+  const languages = Object.values(dataArr[0].languages).join(', ');
   oneCountryInfo.innerHTML = 
-  `<h1><img width=15 src="${dataArr[0].flags.svg}" alt="флаг країни ${dataArr[0].name.official}"> ${dataArr[0].name.official}</h1>
-      <p>
+    `<div class='one-card'>
+    <h1><img class="country-flag" src="${dataArr[0].flags.svg}" alt="флаг країни ${dataArr[0].name.official}"> ${dataArr[0].name.official}</h1>
+      <div class="info-part">
         <h2>Capital:</h2>
-        <span>${dataArr[0].name.official}</span>
-      </p>
-      <p>
+        <span>${dataArr[0].capital}</span>
+      </div>
+      <div class="info-part">
         <h2>Population:</h2>
         <span>${dataArr[0].population}</span>
-      </p>
-      <p>
+      </div>
+      <div class="info-part">
         <h2>Languages:</h2>
-        <span>${dataArr[0].languages}</span>
-      </p>`
+        <span>${languages}</span>
+      </div>
+    </div>`
 };
+
+function resetMarkup() {
+  countryMatchedList.innerHTML = '';
+  oneCountryInfo.innerHTML = ''
+}
